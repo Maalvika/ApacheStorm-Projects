56d5d672092c647c3854fba35e69aa792204d8c1
Maalvika <maalvika.bachani@colostate.edu>
2017-10-29 17:53:16 -0600

    Fixed

--- TwitterTopics/src/Topology/TwitterMain.java
+++ TwitterTopics/src/Topology/TwitterMain.java
@@ -2,6 +2,7 @@ package Topology;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
@@ -14,12 +15,12 @@ import twitterBolts.WordBolt;
 import twitterSpouts.SentenceSpout;
 import twitterSpouts.TwitterSpout;
 
-public class WCMain {
+public class TwitterMain {
 	private static final String TWITTER_SPOUT_ID = "twitter-spout";
 	private static final String HASHTAG_BOLT_ID = "hashtag-bolt";
 	private static final String COUNT_BOLT_ID = "count-bolt";
 	private static final String OUTPUT_BOLT_ID = "output-bolt";
-	private static final String TOPOLOGY_NAME = "word-count-topology";
+	private static final String TOPOLOGY_NAME = "twitter-count-topology";
 	
 	public static void main(String args[]) {
 		TwitterSpout ss = new TwitterSpout();
@@ -38,12 +39,28 @@ public class WCMain {
 		builder.setBolt(OUTPUT_BOLT_ID, ob).globalGrouping(COUNT_BOLT_ID);
 		
 		Config config = new Config();
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology(TOPOLOGY_NAME, config, builder.
+		config.setDebug(false);
+		config.put(config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+		if(args == null || args.length == 0) {
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(TOPOLOGY_NAME, config, builder.
 				createTopology());
-		Utils.sleep(10);
-		cluster.killTopology(TOPOLOGY_NAME);
-		cluster.shutdown();
+			try {
+				
+				System.out.println("----------------ENTERRING INTO SLEEP MODE---------------");
+				Utils.sleep(10000);
+			} catch(Exception e){
+				System.out.println("------------- exitting sleep mode-----------");
+				e.printStackTrace();
+			}
+		
+			cluster.killTopology(TOPOLOGY_NAME);
+			cluster.shutdown();
+		} else if (args[0].equals("remote")) {
+			config.setNumWorkers(3);
+			StormSubmitter.submitTopologyWithProgressBar(TOPOLOGY_NAME, config, builder.createTopology());
+		}
+	}
 	}
 
 	
